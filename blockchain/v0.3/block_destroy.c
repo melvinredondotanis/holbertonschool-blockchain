990eb220b9b1260a3500f82dcc0d78a514e4d782
Melvin Redondo--Tanis <melvin@redondotanis.com>
2026-01-18 19:18:40 +0100

    add v0.1 to v0.3 parts

---
+++ blockchain/v0.3/block_destroy.c
@@ -0,0 +1,16 @@
+#include "blockchain.h"
+
+/**
+ * block_destroy - frees a block
+ * @block: pointer to block to free
+ */
+void block_destroy(block_t *block)
+{
+	if (block == NULL)
+	{
+		return;
+	}
+
+	llist_destroy(block->transactions, 1, (node_dtor_t)transaction_destroy);
+	free(block);
+}
---
+++ blockchain/v0.3/block_hash.c
@@ -0,0 +1,48 @@
+#include "blockchain.h"
+
+/**
+ * hash_tx_ids - llist action func to hash outputs
+ * @node: transaction_t* struct
+ * @idx: index of node
+ * @arg: pointer to address to write to
+ * Return: 0 if success else 1
+ */
+int hash_tx_ids(llist_node_t node, unsigned int idx, void *arg)
+{
+	(void) idx;
+
+	memcpy(*(uint8_t **)arg, ((transaction_t *)node)->id, SHA256_DIGEST_LENGTH);
+	*(uint8_t **)arg += SHA256_DIGEST_LENGTH;
+	return (0);
+}
+
+
+/**
+ * block_hash - computes hash of block
+ * @block: pointer to block to hash
+ * @hash_buf: buffer to store hash/digest
+ * Return: pointer to buffer
+ */
+uint8_t *block_hash(block_t const *block,
+	uint8_t hash_buf[SHA256_DIGEST_LENGTH])
+{
+	size_t len0 = sizeof(block->info) + block->data.len, len = 0;
+	int8_t *_buf = NULL, *buf = NULL;
+
+	len = len0;
+
+	if (llist_size(block->transactions) > 0)
+		len += llist_size(block->transactions) * SHA256_DIGEST_LENGTH;
+	buf = _buf = calloc(1, len);
+	if (buf == NULL)
+	{
+		return (NULL);
+	}
+	memcpy(buf, &block->info, len0);
+	buf += len0;
+	llist_for_each(block->transactions, hash_tx_ids, &buf);
+	sha256(_buf, len, hash_buf);
+	free(_buf);
+
+	return (hash_buf);
+}
